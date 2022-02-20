66082c278d25d053996cb7eb2cf3ba3c56dd08a6
Quentin Philipp <philippquentin6@gmail.com>
2022-02-20 17:25:12 +0100

    Animation link to projects

--- portfolio/components/ProjectItem.jsx
+++ portfolio/components/ProjectItem.jsx
@@ -39,19 +39,25 @@ const ProjectItem = ({ project }) => (
             </div>
             <div className={styles.buttonContainer}>
                 <Link as={`/project/${project.attributes.slug}`} href="/project/[id]" target="_blank" passHref>
-                    <div className={`${styles.buttonViewProject} ${styles.blue}`}>
+                    <div className={`${styles.buttonViewProject} ${styles.whiteColor}`}>
+                        <div className={styles.textSVGContainer}>
                         View project
-                        <svg fill="#003249" viewBox="0 0 20 20" className={styles.buttonArrow}>
-                            <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
+                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.buttonArrow} width="20px" height="20px" viewBox="0 0 16 16">
+                            <path fill="#003249" className={styles.buttonArrowMain} d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path>
+                            <path className={styles.buttonArrowSecondary} d="M1.75 8H11" stroke-width="1.5" stroke-linecap="round"></path>
                         </svg>
+                        </div>
                     </div>
                 </Link>
                 {project.attributes.demo_url &&
-                <a className={`${styles.buttonViewProject} ${styles.grey}`} href={project.attributes.demo_url} target="_blank" rel="noopener noreferrer" >
-                    Live demo
-                    <svg fill="#003249" viewBox="0 0 20 20" className={styles.buttonArrow}>
-                        <path fillRule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clipRule="evenodd"></path>
-                    </svg>
+                <a className={`${styles.buttonViewProject} ${styles.accentColor}`} href={project.attributes.demo_url} target="_blank" rel="noopener noreferrer" >
+                    <div className={styles.textSVGContainer}>
+                        Live demo
+                        <svg xmlns="http://www.w3.org/2000/svg" className={styles.buttonArrow} width="20px" height="20px" viewBox="0 0 16 16">
+                            <path fill="#003249" className={styles.buttonArrowMain} d="M7.28033 3.21967C6.98744 2.92678 6.51256 2.92678 6.21967 3.21967C5.92678 3.51256 5.92678 3.98744 6.21967 4.28033L7.28033 3.21967ZM11 8L11.5303 8.53033C11.8232 8.23744 11.8232 7.76256 11.5303 7.46967L11 8ZM6.21967 11.7197C5.92678 12.0126 5.92678 12.4874 6.21967 12.7803C6.51256 13.0732 6.98744 13.0732 7.28033 12.7803L6.21967 11.7197ZM6.21967 4.28033L10.4697 8.53033L11.5303 7.46967L7.28033 3.21967L6.21967 4.28033ZM10.4697 7.46967L6.21967 11.7197L7.28033 12.7803L11.5303 8.53033L10.4697 7.46967Z"></path>
+                            <path className={styles.buttonArrowSecondary} d="M1.75 8H11" stroke-width="1.5" stroke-linecap="round"></path>
+                        </svg>
+                    </div>
                 </a>
                 }
 
