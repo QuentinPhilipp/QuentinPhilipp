0c52a014dab0aa83b2aff5c59220750b4114097a
Quentin Philipp <philippquentin6@gmail.com>
2022-02-22 14:07:25 +0100

    Fix click on menu and responsive in project items

--- portfolio/components/shared/Navbar.jsx
+++ portfolio/components/shared/Navbar.jsx
@@ -14,27 +14,34 @@ function Navbar() {
     }
   }
 
+  function turnOffResponsive() {
+    let x = document.getElementById('topnavResp');
+    if (x.classList.contains(`${styles.responsive}`)) {
+      x.classList.remove(`${styles.responsive}`)
+    }
+  }
+
   return (
   <nav className={styles.navbar} id="topnavResp">
       <Link as="/#home" href="/#home" passHref>
-        <span className={styles.navItem}>Home</span>
+        <span className={styles.navItem} onClick={turnOffResponsive}>Home</span>
       </Link>
       <Link as="/#about" href="/#about" passHref>
-        <span className={styles.navItem}>About</span>
+        <span className={styles.navItem} onClick={turnOffResponsive}>About</span>
       </Link>
       <Link as="/#experiences" href="/#experiences" passHref>
-        <span className={styles.navItem}>Experiences</span>
+        <span className={styles.navItem} onClick={turnOffResponsive}>Experiences</span>
       </Link>
       <Link as="/#projects" href="/#projects" passHref>
-        <span className={styles.navItem}>Projects</span>
+        <span className={styles.navItem} onClick={turnOffResponsive}>Projects</span>
       </Link>
-      <a href="#" className={styles.icon} onClick={toggleResponsive}>
+      <p className={styles.icon} onClick={toggleResponsive}>
       <svg viewBox="0 0 100 80" width="30" height="30" fill="#fff">
         <rect width="100" height="17"></rect>
         <rect y="30" width="100" height="17"></rect>
         <rect y="60" width="100" height="17"></rect>
       </svg>
-      </a>
+      </p>
   </nav>
   );
 }
