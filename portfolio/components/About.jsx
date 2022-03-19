85e9d759c01f190e4098a43af6d25fb0ac60a530
Quentin Philipp <philippquentin6@gmail.com>
2022-03-19 21:26:08 +0100

    Remove photo container

--- portfolio/components/About.jsx
+++ portfolio/components/About.jsx
@@ -10,7 +10,7 @@ import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 const About = ({ content, links, profile }) => (
     <section id="about" className={styles.layoutContainer}>
         <div className={styles.layout}>
-            <div className={styles.picture}>
+            {/* <div className={styles.picture}>
             <Image
             src={getStrapiMedia(profile.attributes.image.data.attributes)}
             width={250}
@@ -18,7 +18,7 @@ const About = ({ content, links, profile }) => (
             alt="Profile picture"
             objectFit="cover"
               />
-            </div>
+            </div> */}
             <div className={styles.aboutContent}>
                 <h2 id="projectTitle" className={styles.aboutHeader}>
                 <RoughNotation strokeWidth="3" color="#003249" type="underline" show={true}>About Me</RoughNotation>
